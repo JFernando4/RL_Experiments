dc37e8209ba0dcbba68bc78e1a5b813113db0223
jfernando <jfhernan@ualberta.ca>
2018-04-22 18:05:11 -0600

    updated er buffer class

--- Experiments_Engine/Function_Approximators/Neural_Networks/NN_Utilities/experience_replay_buffer.py
+++ Experiments_Engine/Function_Approximators/Neural_Networks/NN_Utilities/experience_replay_buffer.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Experience_Replay_Buffer():
 
-    def __init__(self, buffer_size=10, batch_size=1, n=1, observation_dimensions=[2,2], num_actions=18,
+    def __init__(self, buffer_size=10, batch_size=1, n=1, observation_dimensions=[2,2],
                  observation_dtype=np.uint8, return_function=None):
         assert return_function is not None, "Please provide a return function for the buffer."
         self._buff_sz = buffer_size
@@ -14,48 +14,50 @@ class Experience_Replay_Buffer():
         self._current_buffer_size = 0
         self._buffer_full = False
         self._return_function = return_function
-        self._safe_indexes = []
-        self._safe_indexes_queue = []
+        self._uptodate = []
+        self._buffer = []
 
+    def store_observation(self, reward, action, q_val, termination, state=np.array()):
+        """
+        Example of an observation in the buffer:
         observation = {
-            "reward": 0.0,
-            "action": 0,
-            "state": np.zeros(shape=self._obs_dim, dtype=self._obs_dtype).tobytes(),
-            "q_val": np.zeros(num_actions, dtype=np.float64),
-            "termination": False,
-            "up_to_date": True
-        }
-        self._buffer = [observation] * self._buff_sz
+             "reward": 0.0,
+             "action": 0,
+             "state": np.zeros(shape=self._obs_dim, dtype=self._obs_dtype).tobytes(),
+             "q_val": np.zeros(num_actions, dtype=np.float64),
+             "termination": False,
+             "up_to_date": True
+         }
+        """
+        observation = {"reward": reward,
+                       "action": action,
+                       "state": state.tobytes(),
+                       "q_val": q_val,
+                       "termination": termination}
+        self._buffer.append(observation)
+        self._uptodate.append(True)
 
-    def store_observation(self, observation=dict()):
-        observation["up_to_date"] = True
-        self._buffer[self._current_buffer_size] = observation
-        self.add_to_safe_indexes_list(self._current_buffer_size)
-
-        self._current_buffer_size += 1
         if self._current_buffer_size >= self._buff_sz:
-            self._current_buffer_size = 0
-            if not self._buffer_full:
-                self._buffer_full = True
+            self._buffer_full = True
+        else:
+            self._current_buffer_size += 1
 
-    def add_to_safe_indexes_list(self, daindex):
-        self._safe_indexes_queue.append(daindex)
-        if len(self._safe_indexes_queue) > self._n:
-            self._safe_indexes.append(self._safe_indexes.pop(0))
-        if len(self._safe_indexes) > (self._buff_sz - self._n):
-            self._safe_indexes.pop(0)
+        if len(self._buffer) > self._buff_sz:
+            self._buffer.pop(0)
+            self._uptodate.pop(0)
 
     def sample_from_buffer(self, update_function=None):
         assert update_function is not None, "You need to provide an update_function."
-        if self._batch_sz > len(self._safe_indexes):
-            raise ValueError("The buffer is not big enough to sample from it.")
-        daindices = np.random.choice(self._safe_indexes, replace=False)
+        if not self._buffer_full:
+            if self._batch_sz > self._current_buffer_size - self._n:
+                raise ValueError("The buffer is not big enough to sample from it.")
+            daindices = np.random.choice(self._current_buffer_size-self._n, replace=False)
+        else:
+            daindices = np.random.choice(self._buff_sz-3, replace=False)
 
         dabatch = []
         for daindex in daindices:
             observation = self.gather_data(daindex, update_function)
-
-
 
     def gather_data(self, daindex, update_function):
         trajectory = []
