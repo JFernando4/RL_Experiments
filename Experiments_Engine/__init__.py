a24dab04e200d8e1380f267b3524e9b0a23e4858
jfernando <jfhernan@ualberta.ca>
2018-04-30 22:43:52 -0600

    added error handling (again...)

--- Experiments_Engine/__init__.py
+++ Experiments_Engine/__init__.py
@@ -1,40 +1,6 @@
+from .Error_Handling_Util import *
 from .Function_Approximators import *
 from .Objects_Bases import *
 from .RL_Algorithms import *
 from .Environments import *
 from .Policies import *
-
-# """ Object bases """
-# from .Objects_Bases.NN_Model_Base import ModelBase
-# from .Objects_Bases.Environment_Base import EnvironmentBase
-# from .Objects_Bases.Policy_Base import PolicyBase
-# from .Objects_Bases.Function_Approximator_Base import FunctionApproximatorBase
-# from .Objects_Bases.RL_Algorithm_Base import RL_ALgorithmBase
-#
-# """ Function Approximators """
-# # TileCoder
-# from .Function_Approximators.TileCoder import Tile_Coding_FA
-#
-# # Neural Networks
-#     # Experience Replay Buffers
-# from .Function_Approximators.Neural_Networks.NN_Utilities.circular_buffer import CircularBuffer
-# from .Function_Approximators.Neural_Networks.Experience_Replay_Buffer.experience_replay_buffer import Experience_Replay_Buffer
-# from .Function_Approximators.Neural_Networks.Experience_Replay_Buffer.qsigma_erp import QSigmaExperienceReplayBuffer
-#     # Neural Network Utilities
-# from .Function_Approximators.Neural_Networks.NN_Utilities.percentile_estimator import Percentile_Estimator
-# from .Function_Approximators.Neural_Networks.NN_Utilities.models import Model_nCPmFO
-# from .Function_Approximators.Neural_Networks.NN_Utilities.models import Model_mFO
-#     # Neural Networks
-# from .Function_Approximators.Neural_Networks.NN_with_Experience_Replay import NeuralNetwork_wER_FA
-# from .Function_Approximators.Neural_Networks.Neural_Network import NeuralNetwork_FA
-#
-# """ Environments """
-# from .Environments.Arcade_Learning_Environment.ALE_Environment import ALE_Environment
-# from .Environments.OG_MountainCar import Mountain_Car
-#
-# """ Policies """
-# from .Policies.Epsilon_Greedy import EpsilonGreedyPolicy
-#
-# """ RL Algorithms """
-# from .RL_Algorithms.return_functions import QSigmaReturnFunction
-# from .RL_Algorithms.Q_Sigma import QSigma
