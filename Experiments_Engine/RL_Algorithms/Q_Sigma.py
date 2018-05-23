8133c519206cf68e81bee96a4c3bb4a2c52a047e
jfernando <jfhernan@ualberta.ca>
2018-05-23 14:46:07 -0600

    clean up some parts of the code and got rid of the sample_run function

--- Experiments_Engine/RL_Algorithms/Q_Sigma.py
+++ Experiments_Engine/RL_Algorithms/Q_Sigma.py
@@ -14,7 +14,6 @@ class QSigma(RL_ALgorithmBase):
         """
         Summary Name: return_per_episode
         """
-
         self.config = config or Config()
         assert isinstance(config, Config)
         """ 
@@ -50,6 +49,7 @@ class QSigma(RL_ALgorithmBase):
 
         # Experience Replay Buffer: used for storing and retrieving observations. Mainly for Deep RL
         self.er_buffer = er_buffer
+        if self.use_er_buffer: assert self.er_buffer is not None
 
         # Function Approximator: used to approximate the Q-Values
         self.fa = function_approximator
@@ -77,23 +77,6 @@ class QSigma(RL_ALgorithmBase):
                                   self.gamma * (1-Sigma_t) * (v_t - pi_t * Q_t[A_t])
         return estimate_return
 
-    def sample_run(self, tpolicy=False, render=True):
-        policy = self.bpolicy
-        self.env.set_render(render)
-        if tpolicy:
-            policy = self.tpolicy
-
-        self.env.reset()
-        terminate = False
-        S = self.env.get_current_state()
-        while not terminate:
-            q_values = self.fa.get_next_states_values(S)
-            A = policy.choose_action(q_values)
-            S, _, terminate = self.env.update(A)
-
-        self.env.set_render()
-        self.env.reset()
-
     def adjust_sigma(self):
         self.config.sigma *= self.beta
         self.sigma = self.config.sigma
@@ -103,12 +86,9 @@ class QSigma(RL_ALgorithmBase):
 
         Actions = zeros(self.n + 1, dtype=int)
         States = [[] for _ in range(self.n + 1)]
-
         for episode in range(num_episodes):
             # Record Keeping
             episode_reward_sum = 0
-            # self._agent_dictionary['episode_number'] += 1
-
             # Current State, Action, and Q_values
             S = self.env.get_current_state()
             q_values = self.fa.get_next_states_values(S)
@@ -119,7 +99,6 @@ class QSigma(RL_ALgorithmBase):
             else:
                 A = np.random.randint(len(q_values))
                 self.config.rand_steps_count += 1
-
             # Storing in the experience replay buffer
             if self.use_er_buffer:
                 observation = {"reward": 0, "action":A, "state":self.env.get_state_for_er_buffer(), "terminate": False,
@@ -128,30 +107,23 @@ class QSigma(RL_ALgorithmBase):
                 self.er_buffer.store_observation(observation)
             T = inf
             t = 0
-
             # Storing
             States[t % (self.n + 1)] = S
             Actions[t % (self.n + 1)] = A
-
             # Trajectory
             trajectory = []
-
             while 1:
                 if t < T:
                     # Step in the environment
                     S, R, terminate = self.env.update(A)
-
                     # Updating Q_values and State
                     States[(t+1) % (self.n+1)] = S
                     q_values = self.fa.get_next_states_values(S)
-
                     # Record Keeping
                     episode_reward_sum += R
-
                     if terminate:
                         T = t + 1
-                        bpropabilities = np.ones\
-                            (self.env.get_num_actions(), dtype=np.float64)
+                        bpropabilities = np.ones(self.env.get_num_actions(), dtype=np.float64)
                         A = np.uint8(0)
                     else:
                         if self.config.rand_steps_count >= self.initial_rand_steps:
@@ -165,34 +137,28 @@ class QSigma(RL_ALgorithmBase):
                             self.config.rand_steps_count += 1
 
                         Actions[(t + 1) % (self.n + 1)] = A
-
                     # Storing Trajectory
                     trajectory.append([R, A, q_values, terminate])
-
                     # Storing in the experience replay buffer
                     if self.use_er_buffer:
                         observation = {"reward": R, "action": A, "state": self.env.get_state_for_er_buffer(),
                                        "terminate": terminate, "rl_return": np.nan, "uptodate": False,
                                        "bprobabilities": bpropabilities, "sigma": self.sigma}
                         self.er_buffer.store_observation(observation)
-
+                # Computing the return and updating the function approximator
                 tau = t - self.n + 1
                 if tau >= 0:
                     if len(trajectory) >= 1:
-                        temp_copy_of_trajectory = list(trajectory)
                         if not self.use_er_buffer:
-                            # G = self.recursive_return_function(temp_copy_of_trajectory)
                             G = self.iterative_return_function(trajectory)
                         else:   # No need to compute the return if we're using experience replay
                             G = 0
                         if self.config.rand_steps_count >= self.initial_rand_steps:
                             self.fa.update(States[tau % (self.n+1)], Actions[tau % (self.n+1)], nstep_return=G)
                         trajectory.pop(0)
-
                 t += 1
                 if tau == T-1: break
-
-            if self.save_summary:
-                self.summary['return_per_episode'].append(episode_reward_sum)
+            # End of episode
+            if self.save_summary: self.summary['return_per_episode'].append(episode_reward_sum)
             self.adjust_sigma()
             self.env.reset()
