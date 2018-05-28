bceba37c8afcda764cd4e4f441f1e050f79fa6bd
jfernando <jfhernan@ualberta.ca>
2018-05-27 20:30:03 -0600

    added option to use the sigmas from the buffer or the current sigmas

--- Experiments_Engine/RL_Agents/onpolicy_qsigma_return.py
+++ Experiments_Engine/RL_Agents/onpolicy_qsigma_return.py
@@ -16,12 +16,15 @@ class OnPolicyQSigmaReturnFunction:
         sigma                   float           0.5                 Sigma parameters, see De Asis et. al. (2018)
         sigma_decay             float           1.0                 Decay rate of sigma. At the end of each episode
                                                                     we let: sigma *= sigma_decay
+        use_buffer_sigma        bool            False               Whether to use the sigma retrieved from the buffer
+                                                                    or use the current sigma               
         """
         self.config = config
         self.n = check_attribute_else_default(config, 'n', 1)
         self.gamma = check_attribute_else_default(config, 'gamma', 1.0)
         self.sigma = check_attribute_else_default(config, 'sigma', 0.5)
         self.sigma_decay = check_attribute_else_default(config, 'sigma_decay', 1.0)
+        self.use_buffer_sigma = check_attribute_else_default(config, 'use_buffer_sigma', False)
 
         """
         Other Parameters:
@@ -29,7 +32,7 @@ class OnPolicyQSigmaReturnFunction:
         """
         self.tpolicy = tpolicy
 
-    def batch_iterative_return_function(self, rewards, actions, qvalues, terminations, batch_size):
+    def batch_iterative_return_function(self, rewards, actions, qvalues, terminations, sigmas, batch_size):
         """
         Assumptions of the implementation:
             All the rewards after the terminal state are 0.
@@ -40,9 +43,13 @@ class OnPolicyQSigmaReturnFunction:
         :param actions: expected_shape = [batch_size, n], expected_type = np.uint8, np.uint16, np.uint32, or np.uint64
         :param qvalues: expected_shape = [batch_size, n, num_actions]
         :param terminations: expected_shape = [batch_size, n]
+        :param sigmas: expected_shape = [batch_size, n]
         :param batch_size: dtype = int
         :return: estimated_returns:
         """
+        if not self.use_buffer_sigma:
+            sigmas = np.ones(shape=sigmas.shape, dtype=np.float64) * self.sigma
+
         num_actions = self.tpolicy.num_actions
         tprobabilities = np.ones([batch_size, self.n, self.tpolicy.num_actions], dtype=np.float64)
 
@@ -61,7 +68,7 @@ class OnPolicyQSigmaReturnFunction:
             R_t = rewards[:, i]
             A_t = actions[:, i]
             Q_t = qvalues[:, i, :]
-            Sigma_t = self.sigma
+            Sigma_t = sigmas[:, i]
             gamma = self.gamma
             exec_q = Q_t[batch_idxs, A_t]       # The action-value of the executed actions
             assert np.sum(exec_q == selected_qval[:,i]) == batch_size
