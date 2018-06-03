cf62233f65d84af920060463dbf4cf9fead679ef
jfernando <jfhernan@ualberta.ca>
2018-06-03 13:08:09 -0600

    all the layers in the the Model with RBFLayer are rbf now

--- Experiments_Engine/Function_Approximators/Neural_Networks/NN_Utilities/models.py
+++ Experiments_Engine/Function_Approximators/Neural_Networks/NN_Utilities/models.py
@@ -226,7 +226,7 @@ Creates a model with n convolutinal layers followed by a pooling step and m full
 one linear output layer. The last fully connected layer of the network uses a radial basis function of the form
 f(x) = e^{(x-c)^2 / (1/(# of neurons)), where c is a uniform sample from [0,1] and fixed for the rest of training.
 """
-class Model_nCPmFO_wRBFLayer(ModelBase):
+class Model_nCPmFO_wRBFLayer2(ModelBase):
 
     def __init__(self, config=None, name="default", SEED=None):
         super().__init__()
@@ -370,6 +370,148 @@ class Model_nCPmFO_wRBFLayer(ModelBase):
         return self.train_vars[0]
 
 
+"""
+Creates a model with n convolutinal layers followed by a pooling step and m fully connected layers followed by
+one linear output layer. All the layers of the network use a radial basis function of the form
+f(x) = e^{(x-c)^2 / (1/(# of neurons)), where c is a uniform sample from [-1,1] and fixed for the rest of training and
+x is normalized as norm(x_i) = x_i / abs(max(X)) where X is a matrix and the max is taken over all the rows and columns
+"""
+class Model_nCPmFO_wRBFLayer(ModelBase):
+
+    def __init__(self, config=None, name="default", SEED=None):
+        super().__init__()
+
+        assert isinstance(config, Config)
+        """ 
+        Parameters in config:
+        Name:                   Type:           Default:            Description: (Omitted when self-explanatory)
+        dim_out                 list            [10,10,10]          the output dimensions of each layer, i.e. neurons
+        filter_dims             list            [2,2]               the dimensions of each filter
+        strides                 list            [4, 2]              strides use by each convolutional layer
+        obs_dims                list            [4,84,84]           the dimensions of the observations seen by the agent
+        num_actions             int             2                   the number of actions available to the agent
+        gate_fun                tf gate fun     tf.nn.relu          the gate function used across the whole network
+        conv_layers             int             2                   number of convolutional layers
+        full_layers             int             1                   number of fully connected layers
+        max_pool                bool            True                indicates whether to max pool between each conv layer
+        frames_format           str             "NCHW"              Specifies the format of the frames fed to the network
+        norm_factor             float           1                   Normalizes the frames by the value provided               
+        """
+        self.dim_out = check_attribute_else_default(config, 'dim_out', [10,10,10])
+        self.filter_dims = check_attribute_else_default(config, 'filter_dims', [2,2])
+        self.strides = check_attribute_else_default(config, 'strides', [4,2])
+        channels, height, width = check_attribute_else_default(config, 'obs_dims', [4, 84, 84])
+        num_actions = check_attribute_else_default(config, 'num_actions', 2)
+        self.gate_fun = check_attribute_else_default(config, 'gate_fun', tf.nn.relu)
+        self.convolutional_layers = check_attribute_else_default(config, 'conv_layers', 2)
+        self.fully_connected_layers = check_attribute_else_default(config, 'full_layers', 1)
+        self.max_pool = check_attribute_else_default(config, 'max_pool', True)
+        self.frames_format = check_attribute_else_default(config, 'frames_format', 'NCHW')
+        self.norm_factor = check_attribute_else_default(config, 'norm_factor', 1.)
+
+        """
+        Other Parameters:
+        name - name of the network. Should be a string.
+        """
+        self.name = name
+        row_and_action_number = 2
+        total_layers = self.convolutional_layers + self.fully_connected_layers
+
+        " Placehodler "
+        self.x_frames = tf.placeholder(tf.float32, shape=(None, channels, height, width))   # input frames
+        self.x_frames = tf.divide(self.x_frames, self.norm_factor)
+        self.x_actions = tf.placeholder(tf.int32, shape=(None, row_and_action_number))      # input actions
+        self.y = tf.placeholder(tf.float32, shape=None)                                     # target
+
+        " Variables for Training "
+        self.train_vars = []
+
+        """ Convolutional layers """
+        dim_in_conv = [channels] + self.dim_out[:self.convolutional_layers - 1]
+        current_s_hat = self.x_frames
+        if self.frames_format == "NHWC":
+            current_s_hat = tf.transpose(current_s_hat, [0, 2, 3, 1])
+
+        for i in range(self.convolutional_layers):
+            if self.frames_format == "NHWC":
+                out_height = np.ceil(current_s_hat.shape[1]._value / self.strides[i])
+                out_width = np.ceil(current_s_hat.shape[2]._value / self.strides[i])
+                centers_shape = np.array((out_height, out_width, self.dim_out[i]), dtype=np.uint32)
+            else:
+                out_height = np.ceil(current_s_hat.shape[2]._value / self.strides[i])
+                out_width = np.ceil(current_s_hat.shape[3]._value /self.strides[i])
+                _, channels, height, width = current_s_hat.shape
+                centers_shape = np.array((self.dim_out[i], out_height, out_width), dtype=np.uint32)
+            centers = tf.constant(np.random.uniform(low=-1, high=1, size=centers_shape), dtype=np.float32)
+            stddev = tf.constant(tf.divide(1, np.prod(centers_shape)), dtype=np.float32)
+            # layer n: convolutional
+            W, b, z_hat, r_hat = layers.convolution_2d_rbf(
+                self.name, "conv_rbf_"+str(i+1), current_s_hat, self.filter_dims[i], dim_in_conv[i], self.dim_out[i],
+                tf.random_normal_initializer(stddev=1.0 / np.sqrt(self.filter_dims[i]**2 * dim_in_conv[i] + 1),
+                                             seed=SEED),
+                center=centers, stddev=stddev, stride=self.strides[i], format=self.frames_format)
+            # layer n + 1/2: pool
+            if self.max_pool:
+                s_hat = tf.nn.max_pool(
+                    r_hat, ksize=[1, 1, 2, 2], strides=[1, 1, 2, 2], padding="SAME")
+            else:
+                s_hat = r_hat
+
+            current_s_hat = s_hat
+            self.train_vars.extend([W, b])
+
+        """ Fully Connected layers """
+        shape = current_s_hat.get_shape().as_list()
+        current_y_hat = tf.reshape(current_s_hat, [-1, shape[1] * shape[2] * shape[3]])
+        # shape[-3:] are the last 3 dimensions. Shape has 4 dimensions: dim 1 = None, dim 2 =
+        dim_in_fully = [np.prod(shape[-3:])] + self.dim_out[self.convolutional_layers: total_layers-1]
+        dim_out_fully = self.dim_out[self.convolutional_layers:]
+        for j in range(self.fully_connected_layers):
+            centers_shape = (dim_in_fully[j], dim_out_fully[j])
+            centers = tf.constant(np.random.uniform(low=-1, high=1, size=centers_shape), dtype=np.float32)
+            stddev = tf.constant(tf.divide(1, np.prod(centers_shape)), dtype=np.float32)
+
+            # layer n + m: fully connected
+            W, b, z_hat, y_hat = layers.fully_connected_rbf(
+                self.name, "full_rbf_"+str(j+1), current_y_hat, dim_in_fully[j], dim_out_fully[j],
+                tf.random_normal_initializer(stddev=1.0 / np.sqrt(dim_in_fully[j]), seed=SEED),
+                center=centers, stddev=stddev)
+
+            current_y_hat = y_hat
+            self.train_vars.extend([W, b])
+
+        """ Output layer """
+        # output layer: fully connected
+        W, b, z_hat, self.y_hat = layers.fully_connected(
+            self.name, "output_layer", current_y_hat, self.dim_out[-1], num_actions,
+            tf.random_normal_initializer(stddev=1.0 / np.sqrt(self.dim_out[-1]), seed=SEED), linear_transfer)
+        self.train_vars.extend([W, b])
+        self.train_vars = [self.train_vars]
+
+        # Obtaining y_hat and Scaling by the Importance Sampling
+        y_hat = tf.gather_nd(self.y_hat, self.x_actions)
+        y = self.y
+        # Temporal Difference Error
+        self.td_error = tf.subtract(y, y_hat)
+        # Loss
+        self.train_loss = tf.reduce_sum(tf.pow(self.td_error, 2))
+
+    def replace_model_weights(self, new_vars, tf_session=tf.Session()):
+        if not isinstance(new_vars, list):
+            new_vars = [new_vars]
+        assert len(new_vars) == len(self.train_vars[0]), "The lists of variables need to have the same length!"
+
+        for i in range(len(self.train_vars[0])):
+            tf_session.run(tf.assign(self.train_vars[0][i], new_vars[i]))
+
+    def get_variables_as_list(self, tf_session=tf.Session()):
+        var_list = []
+        for i in range(len(self.train_vars[0])):
+            var_list.append(tf_session.run(self.train_vars[0][i]))
+        return var_list
+
+    def get_variables_as_tensor(self):
+        return self.train_vars[0]
 
 ########################################################################################################################
 ##########################                    Old Code                            ######################################
